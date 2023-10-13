09c852a3d81313a2192ec876205a94911e18d7f8
ZairGalindo <112458425+ZairGalindo@users.noreply.github.com>
2023-10-12 21:53:23 -0600

    Creación  De Practica

---
+++ src/poop_1/POOP_1.java
@@ -0,0 +1,16 @@
+package poop_1;
+
+/**
+ * El metodo Main es el punto de entrada del programa.
+ * Aqui se inicia la ejecucion del programa.
+ * 
+ * @author Johann Zair Galindo Mayer
+ */
+public class POOP_1 {
+
+    public static void main(String[] args) {
+        // Imprime ¡Hola Mundo! en la consola
+        System.out.println("¡Hola Mundo Desde POO Grupo 8!");
+    }
+    
+}
