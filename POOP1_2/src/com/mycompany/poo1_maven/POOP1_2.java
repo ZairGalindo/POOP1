c3d8878bc0cd617fb335ee21bcdf9015f3206b5d
ZairGalindo <112458425+ZairGalindo@users.noreply.github.com>
2023-10-12 22:00:26 -0600

    Add files via upload

---
+++ POOP1_2/src/com/mycompany/poo1_maven/POOP1_2.java
@@ -0,0 +1,12 @@
+package com.mycompany.poo1_maven;
+
+/**
+ *
+ * @author Galindo Mayer Johann Zair
+ */
+public class POOP1_2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo desde POO grupo 8 desde Maven");
+    }
+}
